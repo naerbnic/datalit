181d7c552e10c82f22cf1d8a81fa8f45306a3ea7
Brian Chin <brian.chin@gmail.com>
2025-09-10 12:23:08 -0700

    Set a common initial alpha version for all crates in workspace, and reformat directories

--- datalit-macros/src/lib.rs
+++ datalit-macros/src/lib.rs
@@ -1,5 +1,6 @@
 use proc_macro::TokenStream as BaseTokenStream;
 
+/// The `datalit` macro allows generating embedded data
 #[proc_macro]
 pub fn datalit(input: BaseTokenStream) -> BaseTokenStream {
     datalit_macros_internals::generate_expr_raw(input.into()).into()
